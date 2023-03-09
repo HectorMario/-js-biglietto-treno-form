d4394ff0e4e132c5a834a1b4fda1e2ff38bbf753
HectorMario <hector.mario.peraz@gmail.com>
2023-03-09 17:42:27 +0100

    balisimo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 const dati = document.getElementById("envia")
 
+
+
+
 dati.addEventListener("click", function(){
-    const kilo = document.getElementById("distanza");
-    const age = document.getElementById("eta").value;
-    const prezzo = 0.21 * kilo.value;
+    let kilo = document.getElementById("distanza");
+    let agi = document.getElementById("eta");
+    let age = agi.value;
+    let prezzo = 0.21 * kilo.value;
+    let nime = document.getElementById("name");
+    let nome = nime.value;
     console.log(prezzo);
     console.log(age);
     if(isNaN(prezzo)){
@@ -13,20 +19,54 @@ dati.addEventListener("click", function(){
         mensage = "Si prega di mettere solo numeri"
         document.getElementById("price").innerHTML = mensage;
     } else {
-        finalPrezzo = ""
-        mensage = ""
+        let offer = ""
+        let finalPrezzo = ""
+        let mensage = ""
+        let codice = Math.round(Math.random() * 10000);
+        let bin = Math.round(Math.random() * 10 + 1);
         if(age < 18){
-            finalPrezzo = ((prezzo) - (prezzo * 0.2)).toFixed(2);
+            finalPrezzo = ((prezzo) - (prezzo * 0.2)).toFixed(2)
+            offer = "Biglietto Minoreni"
         } else if(age>=65){
-            finalPrezzo = ((prezzo) - (prezzo * 0.4)).toFixed(2);
+            finalPrezzo = ((prezzo) - (prezzo * 0.4)).toFixed(2)
+            offer = "Biglietto Anciani"
         } else{
-            finalPrezzo = prezzo.toFixed(2);
+            finalPrezzo = prezzo.toFixed(2)
+            offer = "Biglietto Standard"
         }
         
         console.log(finalPrezzo)
-        document.getElementById("price").innerHTML = "Devi Pagare $" + finalPrezzo;
+        document.getElementById("price").innerHTML ="$" + finalPrezzo;
+        document.getElementById("user").innerHTML = nome;
+        document.getElementById("offert").innerHTML = offer;
+        document.getElementById("code").innerHTML = codice;
+        document.getElementById("carro").innerHTML = bin;
     }
+
+    const resit = document.getElementById("borra");
+
+    resit.addEventListener("click", function(){
+        let offer = "";
+        kilo.value = "";
+        nime.value = "";
+        let prezzo = "";
+        let nome = "";
+        let finalPrezzo = "";
+        let mensage = "";
+        let codice = "";
+        let bin = "";
+        agi.value = "";
+        document.getElementById("price").innerHTML = finalPrezzo;
+        document.getElementById("user").innerHTML = nome;
+        document.getElementById("offert").innerHTML = offer;
+        document.getElementById("code").innerHTML = codice;
+        document.getElementById("carro").innerHTML = bin;
+        document.getElementById("distanza").innerHTML = kilo;
+        document.getElementById("eta").innerHTML = codice;
+        document.getElementById("name").innerHTML = bin;
+    })
     
 })
+
 
 
